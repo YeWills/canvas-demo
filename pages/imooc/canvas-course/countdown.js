f408095947b21e90a1f7bab2fccb873b0cb87b12
hz-qq2 <601661706@qq.com>
2019-12-09 09:03:48 +0800

    倒计时demo 注释

--- pages/imooc/canvas-course/countdown.js
+++ pages/imooc/canvas-course/countdown.js
@@ -37,8 +37,9 @@ function getCurrentShowTimeSeconds() {
     return ret >= 0 ? ret : 0;
 }
 
+//更新落地运动的球 数据，不渲染，渲染交给render函数
 function update(){
-
+//1. 更新数字
     var nextShowTimeSeconds = getCurrentShowTimeSeconds();
 
     var nextHours = parseInt( nextShowTimeSeconds / 3600);
@@ -74,6 +75,7 @@ function update(){
         curShowTimeSeconds = nextShowTimeSeconds;
     }
 
+//2. 更新球
     updateBalls();
 
     // console.log( balls.length)
@@ -86,18 +88,20 @@ function updateBalls(){
         balls[i].x += balls[i].vx;
         balls[i].y += balls[i].vy;
         balls[i].vy += balls[i].g;
-
+    //当球面落到地面时，小球应该反弹原来高度的0.75
         if( balls[i].y >= WINDOW_HEIGHT-RADIUS ){
             balls[i].y = WINDOW_HEIGHT-RADIUS;
             balls[i].vy = - balls[i].vy*0.75;
         }
     }
 
+//只取屏幕内的元素
     var cnt = 0
-    for( var i = 0 ; i < balls.length ; i ++ )
+    for( var i = 0 ; i < balls.length ; i ++ ){
         if( balls[i].x + RADIUS > 0 && balls[i].x -RADIUS < WINDOW_WIDTH )
-            balls[cnt++] = balls[i]
-
+        balls[cnt++] = balls[i]
+    }
+//balls数组前面的元素，都是最先生成的，因此，也应该最先被删除
     while( balls.length > cnt ){
         balls.pop();
     }
@@ -112,6 +116,7 @@ function addBalls( x , y , num ){
                     x:x+j*2*(RADIUS+1)+(RADIUS+1),
                     y:y+i*2*(RADIUS+1)+(RADIUS+1),
                     g:1.5+Math.random(),
+                    //Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4 其实就是-4
                     vx:Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4,
                     vy:-5,
                     color: colors[ Math.floor( Math.random()*colors.length ) ]
@@ -129,6 +134,7 @@ function render( cxt ){
     var minutes = parseInt( (curShowTimeSeconds - hours * 3600)/60 )
     var seconds = curShowTimeSeconds % 60
 
+    //渲染时间数字
     renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
     renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
     renderDigit( MARGIN_LEFT + 30*(RADIUS+1) , MARGIN_TOP , 10 , cxt )
@@ -138,6 +144,7 @@ function render( cxt ){
     renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
     renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
 
+     //渲染落地运动的抛物线球
     for( var i = 0 ; i < balls.length ; i ++ ){
         cxt.fillStyle=balls[i].color;
 
@@ -149,6 +156,7 @@ function render( cxt ){
     }
 }
 
+//数字矩阵，只要知道左上角起点坐标，里面每个元素的坐标都可以确定出来
 function renderDigit( x , y , num , cxt ){
 
     cxt.fillStyle = "rgb(0,102,153)";
